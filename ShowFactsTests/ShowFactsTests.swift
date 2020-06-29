aedd5532125dba0f009f388fde6bb819c31b4793
sachithra wishwamal <sachi.swg@gmail.com>
2020-06-29 19:35:46 +0530

    Initial Commit

---
+++ ShowFactsTests/ShowFactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShowFactsTests.swift
+//  ShowFactsTests
+//
+//  Created by sachithra wishwamal on 6/29/20.
+//  Copyright © 2020 sachithra wishwamal. All rights reserved.
+//
+
+import XCTest
+@testable import ShowFacts
+
+class ShowFactsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
